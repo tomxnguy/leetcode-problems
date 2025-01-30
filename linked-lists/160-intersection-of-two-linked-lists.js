8eee99e02f41d3cd5f2379fc7cf7af75f1868ff0
Thomas Nguyen <tomxnguy@gmail.com>
2025-01-30 12:01:16 -0800

    finished intersection of two linked lists

---
+++ linked-lists/160-intersection-of-two-linked-lists.js
@@ -0,0 +1,42 @@
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+Custom Judge:
+
+The inputs to the judge are given as follows (your program is not given these inputs):
+
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    if (!headA  || !headB) return null;
+    let currA = headA;
+    let currB = headB;
+
+    while (currA !== currB){
+        currA = currA ? currA.next : headB;
+        currB = currB ? currB.next : headA;
+    }
+    return currA;
+};
