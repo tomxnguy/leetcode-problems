ffc71cca498c24e80a490254b1a5f7f6f587262b
Thomas Nguyen <tomxnguy@gmail.com>
2025-03-04 23:18:03 -0800

    valid parentheses

---
+++ classroom-problems/valid-parentheses.js
@@ -0,0 +1,29 @@
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+function isValid(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char in map) {
+            const top = stack.pop();
+            if (top !== map[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
